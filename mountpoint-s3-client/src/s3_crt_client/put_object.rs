c3f7606c793c3cf9abf944caf40a0a93c406d60c
Alessandro Passaro <alexpax@amazon.co.uk>
2024-04-12 15:39:36 +0000

    Wait for CreateMPU on open
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3-client/src/s3_crt_client/put_object.rs
+++ mountpoint-s3-client/src/s3_crt_client/put_object.rs
@@ -21,80 +21,86 @@ impl S3CrtClient {
         key: &str,
         params: &PutObjectParams,
     ) -> ObjectClientResult<S3PutObjectRequest, PutObjectError, S3RequestError> {
-        let span = request_span!(self.inner, "put_object", bucket, key);
-        let mut message = self
-            .inner
-            .new_request_template("PUT", bucket)
-            .map_err(S3RequestError::construction_failure)?;
-
-        let key = format!("/{}", key);
-        message
-            .set_request_path(&key)
-            .map_err(S3RequestError::construction_failure)?;
-
-        if params.trailing_checksums {
-            let checksum_config = ChecksumConfig::trailing_crc32c();
-            message.set_checksum_config(Some(checksum_config));
-        }
-
         let review_callback = ReviewCallbackBox::default();
-        let callback = review_callback.clone();
-
-        if let Some(storage_class) = params.storage_class.to_owned() {
-            message
-                .set_header(&Header::new("x-amz-storage-class", storage_class))
-                .map_err(S3RequestError::construction_failure)?;
-        }
-        if let Some(sse) = params.server_side_encryption.as_ref() {
-            message
-                .set_header(&Header::new(SSE_TYPE_HEADER_NAME, sse))
-                .map_err(S3RequestError::construction_failure)?;
-        }
-        if let Some(key_id) = params.ssekms_key_id.as_ref() {
-            message
-                .set_header(&Header::new(SSE_KEY_ID_HEADER_NAME, key_id))
-                .map_err(S3RequestError::construction_failure)?;
-        }
-        // Variable `response_headers` will be accessed from different threads: from CRT thread which executes `on_headers` callback
-        // and from our thread which executes `review_and_complete`. Callback `on_headers` is guaranteed to finish before this
-        // variable is accessed in `review_and_complete` (see `S3HttpRequest::poll` implementation).
         let response_headers: Arc<Mutex<Option<Headers>>> = Default::default();
-        let response_headers_writer = response_headers.clone();
-        let on_headers = move |headers: &Headers, _: i32| {
-            *response_headers_writer.lock().unwrap() = Some(headers.clone());
-        };
-        let mut options = S3CrtClientInner::new_meta_request_options(message, MetaRequestType::PutObject);
-        options.send_using_async_writes(true);
-        options.on_upload_review(move |review| callback.invoke(review));
+        let (mpu_created_sender, mpu_created) = oneshot::channel::<Result<(), S3RequestError>>();
 
-        // Before the first write, we need to await for the multi-part upload to be created, so we can report errors.
-        // To do so, we need to detect one of two events (whichever comes first):
-        // * a CreateMultipartUpload request completes successfully (potentially after a number of retries),
-        // * the meta-request fails.
-        let (mpu_created_sender, mpu_created) = oneshot::channel();
-        let on_mpu_created_sender = Arc::new(Mutex::new(Some(mpu_created_sender)));
-        let on_error_sender = on_mpu_created_sender.clone();
+        let body = {
+            let span = request_span!(self.inner, "put_object", bucket, key);
+            let mut message = self
+                .inner
+                .new_request_template("PUT", bucket)
+                .map_err(S3RequestError::construction_failure)?;
 
-        let body = self.inner.make_simple_http_request_from_options(
-            options,
-            span,
-            move |metrics| {
-                if metrics.request_type() == RequestType::CreateMultipartUpload && !metrics.error().is_err() {
-                    // Signal that a CreateMultipartUpload completed successfully (unless the meta-request had already failed).
-                    if let Some(sender) = on_mpu_created_sender.lock().unwrap().take() {
-                        _ = sender.send(Ok(()));
+            let key = format!("/{}", key);
+            message
+                .set_request_path(&key)
+                .map_err(S3RequestError::construction_failure)?;
+
+            if params.trailing_checksums {
+                let checksum_config = ChecksumConfig::trailing_crc32c();
+                message.set_checksum_config(Some(checksum_config));
+            }
+
+            let callback = review_callback.clone();
+
+            if let Some(storage_class) = params.storage_class.to_owned() {
+                message
+                    .set_header(&Header::new("x-amz-storage-class", storage_class))
+                    .map_err(S3RequestError::construction_failure)?;
+            }
+            if let Some(sse) = params.server_side_encryption.as_ref() {
+                message
+                    .set_header(&Header::new(SSE_TYPE_HEADER_NAME, sse))
+                    .map_err(S3RequestError::construction_failure)?;
+            }
+            if let Some(key_id) = params.ssekms_key_id.as_ref() {
+                message
+                    .set_header(&Header::new(SSE_KEY_ID_HEADER_NAME, key_id))
+                    .map_err(S3RequestError::construction_failure)?;
+            }
+            // Variable `response_headers` will be accessed from different threads: from CRT thread which executes `on_headers` callback
+            // and from our thread which executes `review_and_complete`. Callback `on_headers` is guaranteed to finish before this
+            // variable is accessed in `review_and_complete` (see `S3HttpRequest::poll` implementation).
+            let response_headers_writer = response_headers.clone();
+            let on_headers = move |headers: &Headers, _: i32| {
+                *response_headers_writer.lock().unwrap() = Some(headers.clone());
+            };
+            let mut options = S3CrtClientInner::new_meta_request_options(message, MetaRequestType::PutObject);
+            options.send_using_async_writes(true);
+            options.on_upload_review(move |review| callback.invoke(review));
+
+            // Before returning, we need to await for the multi-part upload to be created, so we can report errors.
+            // To do so, we need to detect one of two events (whichever comes first):
+            // * a CreateMultipartUpload request completes successfully (potentially after a number of retries),
+            // * the meta-request fails.
+            let on_mpu_created_sender = Arc::new(Mutex::new(Some(mpu_created_sender)));
+            let on_error_sender = on_mpu_created_sender.clone();
+
+            self.inner.make_simple_http_request_from_options(
+                options,
+                span,
+                move |metrics| {
+                    if metrics.request_type() == RequestType::CreateMultipartUpload && !metrics.error().is_err() {
+                        // Signal that a CreateMultipartUpload completed successfully (unless the meta-request had already failed).
+                        if let Some(sender) = on_mpu_created_sender.lock().unwrap().take() {
+                            _ = sender.send(Ok(()));
+                        }
                     }
-                }
-            },
-            move |result| {
-                // Signal that the meta-request failed (unless a CreateMultipartUpload had already completed successfully).
-                if let Some(sender) = on_error_sender.lock().unwrap().take() {
-                    _ = sender.send(Err(result.crt_error.into()));
-                }
-                None
-            },
-            on_headers,
-        )?;
+                },
+                move |result| {
+                    // Signal that the meta-request failed (unless a CreateMultipartUpload had already completed successfully).
+                    if let Some(sender) = on_error_sender.lock().unwrap().take() {
+                        _ = sender.send(Err(result.crt_error.into()));
+                    }
+                    None
+                },
+                on_headers,
+            )?
+        };
+
+        // Wait for CreateMultipartUpload to complete successfully, or the MPU to fail.
+        mpu_created.await.unwrap()?;
 
         Ok(S3PutObjectRequest {
             body,
@@ -102,7 +108,6 @@ impl S3CrtClient {
             start_time: Instant::now(),
             total_bytes: 0,
             response_headers,
-            pending_create_mpu: Some(mpu_created),
         })
     }
 }
@@ -152,9 +157,6 @@ pub struct S3PutObjectRequest {
     total_bytes: u64,
     /// Headers of the CompleteMultipartUpload response, available after the request was finished
     response_headers: Arc<Mutex<Option<Headers>>>,
-    /// Signal indicating that CreateMultipartUpload completed successfully, or that the MPU failed.
-    /// Set to [None] once awaited on the first write, meaning the MPU was already created or failed.
-    pending_create_mpu: Option<oneshot::Receiver<Result<(), S3RequestError>>>,
 }
 
 fn try_get_header_value(headers: &Headers, key: &str) -> Option<String> {
@@ -166,12 +168,6 @@ impl PutObjectRequest for S3PutObjectRequest {
     type ClientError = S3RequestError;
 
     async fn write(&mut self, slice: &[u8]) -> ObjectClientResult<(), PutObjectError, Self::ClientError> {
-        // On first write, check the pending CreateMultipartUpload.
-        if let Some(create_mpu) = self.pending_create_mpu.take() {
-            // Wait for CreateMultipartUpload to complete successfully, or the MPU to fail.
-            create_mpu.await.unwrap()?;
-        }
-
         // Write will fail if the request has already finished (because of an error).
         self.body
             .meta_request
