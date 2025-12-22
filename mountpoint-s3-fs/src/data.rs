68ba94c974d74d3df171238c3c70b6bb607ef4e7
Alessandro Passaro <alexpax@amazon.co.uk>
2025-12-22 13:57:47 +0000

    [wip] Add minimal abstraction over prefetcher and use it in prefetch_benchmark
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

---
+++ mountpoint-s3-fs/src/data.rs
@@ -0,0 +1,20 @@
+use futures::Future;
+use mountpoint_s3_client::types::ETag;
+
+use crate::checksums::ChecksummedBytes;
+
+mod adapter;
+
+pub use adapter::create_data_layer;
+
+pub trait DataLayer {
+    fn download(&self, bucket: String, key: String, etag: ETag, size: usize) -> impl Download;
+}
+
+pub trait Download: Send {
+    fn read(
+        &mut self,
+        offset: u64,
+        length: usize,
+    ) -> impl Future<Output = Result<ChecksummedBytes, anyhow::Error>> + Send;
+}
